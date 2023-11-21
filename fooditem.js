d73cb7309252fde15f01fb40928bee6f4f56de93
Akashkm325 <124574266+Akashkm325@users.noreply.github.com>
2023-11-21 10:19:15 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price:5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 2,
+    name:'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000, 
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Madeshi',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Madesh',
+    category : 'house',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Madesh',
+    category : 'appartments',
+    rating : 4.3,
+    price: 25000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Madesh',
+    category : 'appartments',
+    rating : 4.3,
+    price: 25000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Madesh',
+    category : 'appartments',
+    rating : 4.3,
+    price: 25000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Madesh',
+    category : 'appartments',
+    rating : 4.3,
+    price: 25000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Madesh',
+    category : 'appartments',
+    rating : 4.3,
+    price: 25000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Madesh',
+    category : 'appartments',
+    rating : 4.3,
+    price: 25000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Madesh',
+    category : 'PG Mens',
+    rating : 4.3,
+    price: 4500,
+    img: 'img/M PG.png',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Madesh',
+    category : 'PG Mens',
+    rating : 4.3,
+    price: 4500,
+    img: 'img/M PG.png',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Madesh',
+    category : 'PG Mens',
+    rating : 4.3,
+    price: 4500,
+    img: 'img/M PG.png',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Madesh',
+    category : 'PG Mens',
+    rating : 4.3,
+    price: 4500,
+    img: 'img/M PG.png',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Madesh',
+    category : 'PG Mens',
+    rating : 4.3,
+    price: 4500,
+    img: 'img/M PG.png',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Madesh',
+    category : 'PG Mens',
+    rating : 4.3,
+    price:4500,
+    img: 'img/M PG.png',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Madesh',
+    age: '32',
+    category : 'PG Mens',
+    rating : 4.3,
+    price: 4500,
+    img:'img/M PG.png',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 4500,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 4000,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Madesh',
+    category : 'PG Womens',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/G PG.png',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 5000,
+    img: 'img/image.png',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 5000,
+    img:  'img/image.png',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 4500,
+    img:  'img/image.png',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 4000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 4000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 25000,
+    img:'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Madesh',
+    category : 'Lease House & Appartments',
+    rating : 4.3,
+    price: 20000,
+    img: 'img/appartment.png',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 1500,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 2000,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Madesha',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 1200,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 1000,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 1500,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 1400,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 2500,
+    img: 'img/lodge.png',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Madesh',
+    category : 'Lodge',
+    rating : 4.3,
+    price: 1500,
+    img:  'img/lodge.png',
+    quantity: 1
+},
+]
+
+export {foodItem};
